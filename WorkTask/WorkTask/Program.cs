0f330649282611d1c097134ddca553011204b986
Klim <klim.gemini@gmail.com>
2018-05-06 21:12:53 +0300

    Next commit

--- WorkTask/WorkTask/Program.cs
+++ WorkTask/WorkTask/Program.cs
@@ -11,10 +11,46 @@ namespace WorkTask
     {
         static void Main(string[] args)
         {
+            List<AbstractCar> cars = new List<AbstractCar>
+            {
+                new SmallCar(5000, 5, 90, 250, "Ford Focus"),
+                new SmallCar(5500, 6, 95, 300, "Shkoda Rapid"),
+                new SmallCar(6000, 5, 100, 320, "Mazda 323"),
 
-            LuxuryCar lux = new LuxuryCar(700000, 22, 240, 460, "Rolls-Royce Phantom", "Закреплен");
-            Console.WriteLine(lux.model);
+                
+
+            };
+            //-------------------------------------------------
+            
+            GetSummaryCost(cars);
+
+            //---------------------------------------
+            //Сортировка по расходу топлива
+            Console.WriteLine("Сортировка автомобилей по расходу топлива:");
+
+            var sortFuel = cars.OrderBy(taxi => taxi.fuel);
+            foreach (AbstractCar taxi in sortFuel)
+            Console.WriteLine($"{taxi.fuel}, л/100км");
+
             Console.ReadKey();
+        }
+
+        //Стоимость парка автомобилей
+        static void GetSummaryCost(List<AbstractCar> cars)
+        {
+            int sum = 0;
+            for (int i = 0; i < cars.Count - 1; i++)
+            {
+                sum += cars[i].cost;
+            }
+            Console.WriteLine("Суммарная стоимость автомобилей");
+            Console.WriteLine($"{sum:### ### ###}, $\n");
+        }
+
+        //Поиск авто по максимальной скорости
+        public static List<AbstractCar> SearchCarMaxSpeed(List <AbstractCar> carValue, int minSpeed, int maxSpeed)
+        {
+            return carValue.Where(t => t.Speed > minSpeed && t.Speed < maxSpeed).ToList();
         }
     }
 }
